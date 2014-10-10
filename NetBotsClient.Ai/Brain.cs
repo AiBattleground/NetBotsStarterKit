d1abe72a7b53c10f4c04f633629d8c4a94130bfb
Pharylon <zach@zachshuford.com>
2014-10-10 15:43:12 -0400

    Saving As I Go.

---
+++ NetBotsClient.Ai/Brain.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using NetBots.Bot.Interface;
+using NetBotsClient.Models;
+
+namespace NetBotsClient.Ai
+{
+    public class Brain : INetBot
+    {
+        public string Name { get { return "Berserker Bot!"; }}
+
+        public IBotLetMoveCollection GetMoves(GameState gameState)
+        {
+            var game = GameStateParser.Parse(gameState);
+
+            //Berserker Bot has pretty simple logic, and is here for demonstration
+            //only. We will loop through each bot you have and make it run straight
+            //toward the enemy spawn.
+            BotletMoveCollection myMoves = new BotletMoveCollection();
+            foreach (var bot in game.Grid.Where(x => x == SquareType.PlayerBot))
+            {
+                var validMoveLocations = bot.GetAdjacentSquares();
+                var enemySpawn = game.Grid.EnemySpawn;
+                var squareToMoveTo = GetNextSquareToTarget(enemySpawn, validMoveLocations);
+                var botletMove = CreateMoveFromSquares(bot, squareToMoveTo);
+                myMoves.Add(botletMove);
+            }
+            return myMoves;
+        }
+
+        public Square GetNextSquareToTarget(Square targetSquare, IEnumerable<Square> possibleMoves)
+        {
+            int shortestDistance = int.MaxValue;
+            Square squareToMoveTo = null;
+            foreach (var square in possibleMoves)
+            {
+                var distance = square.DistanceFrom(targetSquare);
+                if (distance < shortestDistance)
+                {
+                    shortestDistance = distance;
+                    squareToMoveTo = square;
+                }
+            }
+            return squareToMoveTo;
+        }
+
+        public BotletMove CreateMoveFromSquares(Square origin, Square destination)
+        {
+            var move = new BotletMove();
+            move.from = origin.LineIndex;
+            move.to = destination.LineIndex;
+            return move;
+        }
+    }
+}
---
+++ NetBotsClient.Ai/GameStateParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetBots.Bot.Interface;
+using NetBotsClient.Models;
+
+namespace NetBotsClient.Ai
+{
+    class GameStateParser
+    {
+        public static GameBoard Parse(GameState gameState)
+        {
+            var game = new GameBoard
+            {
+                EnemyEnergy = gameState.p2.energy,
+                MyEnergy = gameState.p1.energy,
+                Grid = CreateGrid(gameState),
+                Turn = gameState.turnsElapsed,
+                TurnLimit = gameState.maxTurns
+            };
+            return game;
+        }
+
+        public static Grid CreateGrid(GameState request)
+        {
+            Grid.MapGridValue('r', SquareType.PlayerBot);
+            Grid.MapGridValue('b', SquareType.EnemyBot);
+            Grid.MapGridValue('*', SquareType.Energy);
+            Grid.MapGridValue('x', SquareType.DeathMarker);
+            var grid = new Grid(request.cols, request.rows, request);
+            return grid;
+        }
+    }
+}
